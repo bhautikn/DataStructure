8a385d65c8f225a3290960c25240247bd8777e5b
Bhautik Navdariya <114481071+bhautikn@users.noreply.github.com>
2023-08-15 16:45:49 +0530

    Add files via upload

--- 100_program/p12.java
+++ 100_program/p12.java
@@ -11,6 +11,18 @@ public class p12 {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("enter n: ");
 		int n = sc.nextInt();
-
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i=0;i<n;i++) {
+			System.out.print("Enter no "+(i+1)+": ");
+			int temp = sc.nextInt();
+			sum += temp;
+			min = (min<temp)?min:temp;
+			max = (max>temp)?max:temp;
+		}
+		System.out.println("Minimum is: "+min);
+		System.out.println("Maximum is: "+max);
+		System.out.println("Avarage is: "+(sum/n));
 	}
 }
