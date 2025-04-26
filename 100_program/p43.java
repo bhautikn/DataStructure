3b8e5cae08eb7962418c147d6098af0d7d9162f8
Bhautik Navdariya <114481071+bhautikn@users.noreply.github.com>
2025-04-26 12:55:15 +0530

    Update p43.java

--- 100_program/p43.java
+++ 100_program/p43.java
@@ -25,7 +25,7 @@ public class p43{
 		}
 		int count = 0;
 		for (int i=0;i<n;i++)
-			for (int j=0;j<n;j++)
+			for (int j=i+1;j<n;j++)
 				if (arr[i]+arr[j]==k)
 					count++;
 		System.out.println(count);
